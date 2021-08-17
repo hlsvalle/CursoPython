8c5540aadeae7563be60446c12bbca900ee2cea9
hlsvalle <hlsvalle@gmail.com>
2021-08-17 16:42:00 -0400

    Adicionando dicionario do contator

--- chamar_contadorpalavras.py
+++ chamar_contadorpalavras.py
@@ -1,4 +1,14 @@
 from contador_palavras import contador_letras
 
-lista_palavras = ['cachorro' , 'gato', 'elefante']
-print(contador_letras(lista_palavras))
+lista_palavras = ['cachorro' , 'gato', 'elefante', 'dinossauro']
+
+dic = {}
+lista = contador_letras(lista_palavras)
+i=0
+for x in lista_palavras:
+    dic[x] = lista[i]
+    i += 1
+
+for keys,values in dic.items():
+    print('O animal ' + keys + ' tem ' + str(values) + ' letras')
+
